153402bd849f312b1a658343f8c5a50850583ae0
Hee Soo Kang <alcilia_382@naver.com>
2022-04-13 15:12:08 +0900

    Update 60057.py

--- working_on/60057.py
+++ working_on/60057.py
@@ -1,29 +1,70 @@
-# 문자열 압축
+def slicer(s, unit):
+    slices = []
+    back = ""
+    count = 1
+
+    for i in range(0, len(s), unit):
+        curr = s[i : i + unit]
+        print(f"인덱스: {i}")
+        print(f"현재: {curr}")
+        print(f"이전: {back}")
+        print("--------------")
+
+        try:
+
+            if curr == back:
+                count += 1
+
+            else:
+                # 첫 루프
+                if i == 0:
+                    continue
+
+                elif count == 1:
+                    slices.append(back)
+
+                elif count > 1:
+                    slices.append(str(count) + back)
+
+                # 카운트 초기화
+                count = 1
+
+        finally:
+            back = curr
+
+        print(f"SLICES: {slices}")
+        print(count)
+        print("--------------")
+
+    # 마지막 루프
+    if count == 1:
+        slices.append(back)
+    else:
+        slices.append(str(count) + back)
+
+    return slices
 
 
 def solution(s):
-    answer = 0
-    # 연속단위 자르기
-    c = []
-    temp = []
-    for i, v in enumerate(s):
-        print(i)
-        if i == len(s) - 1:
-            c.append("".join(temp))
-            print("끝!")
-        else:
-            if len(temp) == 0 or v == temp[-1]:
-                temp.append(v)
-                print(temp)
-            else:
-                c.append("".join(temp))
-                print(c)
-                temp = [v]
+    answer = []
+    if len(s) <= 1:
+        return 1
 
-    print(c)
+    else:
+        for unit in range(1, len(s)):
+            answer.append(len("".join(slicer(s, unit))))
 
-    return answer
+        print(f"후보: {answer}")
+
+        return min(answer)
 
 
-a = solution("aabbaccc")
-# print(a)
+# s = "aabbaccc"
+# s = "ababcdcdababcdcd"
+# s = "abcabcdede"
+s = "abcabcabcabcdededededede"
+# s = "xababcdcdababcdcd"
+# s = "s"
+
+a = solution(s)
+print(a)
